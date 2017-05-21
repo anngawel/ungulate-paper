3ba6297aaea934f7b3b7807c2a90032528009748
anngawel <anngawel@gmail.com>
2017-05-22 09:17:43 +1000

    A whole bunch of stuff - changes to draft, reorganizing, and tables and figures.

--- Analysis/scripts/veg_survey_summarystats.R
+++ Analysis/scripts/veg_survey_summarystats.R
@@ -10,20 +10,36 @@ library(plyr)
 library(ggplot2)
 library(dplyr)
 
-setwd("Analysis/data/working data")
+setwd("~/ungulate-paper/Analysis/data/working data")
 tidy_veg<-read.csv("tidy_veg_gu.csv")
 
+summary(tidy_veg)
+#subset out trees
+trees_gu<-tidy_veg[tidy_veg$morphology=="tree",]
+str(trees_gu)
+trees_gu$age<-factor(trees_gu$age)
 #subset out seedlings
-seedling<-tidy_veg[tidy_veg$age=="seedling",]
-seedling$age<-factor(seedling$age)
+treeseeds<-trees_gu[trees_gu$age=="seedling",]
+str(treeseeds)
+summary(treeseeds)
 
 #Summarize to figure out how many seeds of each spp and what proportion of seedlings each species comprises
-seedsumsite<-ddply(seedling, .(site, spp), summarize, totalseed=sum(numseed))
-seedsum<-ddply(seedling, .(spp), summarize, totalseed=sum(numseed), prop=totalseed/5673)
+#seedsumsite<-ddply(treeseeds, .(site, spp), summarize, totalseed=sum(numseed))
 
-sum(seedsum$totalseed) #5673 seedlings total
+treeseedtotals<-ddply(treeseeds, .(spp), summarize, totalseed=sum(numseed))
+sum(treeseedtotals$totalseed)
+#total tree seedlings 3203
+treeseedtotals$prop<-(treeseedtotals$totalseed/3203)
+str(treeseedtotals)
 
-seedsum$spp<- factor(seedsum$spp, levels = seedsum[order(seedsum$prop, decreasing = TRUE), 1])
+#do the same for totals including adult and seedling trees
+?
 
-ggplot(seedsum, aes(spp, prop))+
-  geom_point()
+# sort by prop
+sort_treesdls <- treeseedtotals[order(-treeseedtotals$prop),]
+
+###trees only - totalseeds = 3203 seedlings total###
+
+ggplot(treeseedtotals, aes(spp, prop))+
+ geom_point()
+
