86144d6161ce0f0808527fe6f7e791376e047aab
anngawel <anngawel@gmail.com>
2017-06-21 18:23:45 +1000

    changes to manuscript and tables

---
+++ Analysis/scripts/hurdle model for scat_attempt.R
@@ -0,0 +1,9 @@
+#################################################
+#####HURDLE MODELS FOR PIGS SCAT DATA#################
+#######LAST MODIFIED BY AMG 19JUN2017################
+#################################################
+
+###load package###
+library(pscl)
+
+###load dataframe###
